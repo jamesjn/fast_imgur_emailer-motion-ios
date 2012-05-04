8f151b3049f0dfd1312111535875247d552cbdf7
James <jc582@optonline.net>
2012-05-04 17:36:10 -0400

    Add camera use

--- app/imgur_uploader_controller.rb
+++ app/imgur_uploader_controller.rb
@@ -2,10 +2,10 @@ class ImgurUploaderController < UIViewController
 
   def viewDidLoad
     @items = []
-    viewImageView = UIImageView.alloc.init
-    viewImageView.image = load_picture_from "http://i.imgur.com/bbg8Y.jpg"
-    viewImageView.frame = [[10,50],[300,200]]
-    view.addSubview(viewImageView)
+    @viewImageView = UIImageView.alloc.init
+    @viewImageView.image = load_picture_from "http://i.imgur.com/bbg8Y.jpg"
+    @viewImageView.frame = [[10,50],[300,200]]
+    view.addSubview(@viewImageView)
 
     button_load = UIButton.buttonWithType(UIButtonTypeRoundedRect)
     button_load.setTitle('Camera', forState:UIControlStateNormal)
@@ -15,12 +15,27 @@ class ImgurUploaderController < UIViewController
   end
 
   def useCamera
+    imagePicker = UIImagePickerController.alloc.init
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceTypeCamera)
+      imagePicker.setSourceType(UIImagePickerControllerSourceTypeCamera)
+    else
+      imagePicker.setSourceType(UIImagePickerControllerSourceTypePhotoLibrary)
+    end
 
+    imagePicker.mediaTypes = [KUTTypeImage]
+    imagePicker.delegate = self
+    presentModalViewController(imagePicker, animated:true)
   end
 
   def viewWillAppear(animated)
     navigationItem.title = 'Imgur file uploader' 
     navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAdd, target:self, action:'seeList')
+  end
+
+  def imagePickerController(picker, didFinishPickingMediaWithInfo:info)
+    image = info.objectForKey(UIImagePickerControllerOriginalImage)
+    @viewImageView.setImage(image)
+    dismissModalViewControllerAnimated(true)
   end
 
   def seeList
